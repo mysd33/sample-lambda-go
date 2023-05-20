fa26a6f65159b6dd5cd5b7a35d5218e2407d95a2
mysd33 <mysd.33@gmail.com>
2023-05-20 10:20:49 +0900

    プリペアードステートメントを使わない実装例も追記

--- app/internal/pkg/repository/user_repository_rdb.go
+++ app/internal/pkg/repository/user_repository_rdb.go
@@ -18,9 +18,17 @@ func (ur *UserRepositoryImplByRDB) GetUser(userId string) (*entity.User, error) 
 	tx := apcontext.Tx
 	ctx := apcontext.Context
 	var user entity.User
-	//row := tx.QueryRow("SELECT user_id, user_name FROM m_user WHERE user_id = $1", userId)
-	//X-RayのSQLトレース対応
+	//プリペアードステートメントによるの例
+	//X-RayのSQLトレースにも対応
 	row := tx.QueryRowContext(ctx, "SELECT user_id, user_name FROM m_user WHERE user_id = $1", userId)
+
+	//プリペアードステートメント未使用の例
+	//RDS Proxy経由で接続する場合、プリペアードステートメントを使用すると、
+	//ピン留め（RDSProxyはコネクションプール内のDB接続を特定のDBクライアントに対して固定）されてしまうことを気にして
+	//回避するため、プレースフォルダを使わない例
+	//https://qiita.com/neruneruo/items/2313feed6d4ce28c2061
+	//row := tx.QueryRowContext(ctx, fmt.Sprintf("SELECT user_id, user_name FROM m_user WHERE user_id = %s", pq.QuoteLiteral(userId)))
+
 	err := row.Scan(&user.ID, &user.Name)
 	if err != nil {
 		return nil, err
@@ -35,9 +43,20 @@ func (ur *UserRepositoryImplByRDB) PutUser(user *entity.User) (*entity.User, err
 
 	tx := apcontext.Tx
 	ctx := apcontext.Context
-	//_, err := tx.Exec("INSERT INTO m_user(user_id, user_name) VALUES($1, $2)", user.ID, user.Name)
-	//X-RayのSQLトレース対応
+	//プリペアードステートメントによる例
+	//X-RayのSQLトレース対応にも対応
 	_, err := tx.ExecContext(ctx, "INSERT INTO m_user(user_id, user_name) VALUES($1, $2)", user.ID, user.Name)
+
+	//プリペアードステートメント未使用の例
+	//RDS Proxy経由で接続する場合、プリペアードステートメントを使用すると、
+	//ピン留め（RDSProxyはコネクションプール内のDB接続を特定のDBクライアントに対して固定）されてしまうことを気にして
+	//回避するため、プレースフォルダを使わない例
+	//https://qiita.com/neruneruo/items/2313feed6d4ce28c2061
+	// SQLインジェクション対策でQuoteLiteralメソッドでエスケープ
+	//userIdParam := pq.QuoteLiteral(user.ID)
+	//userNameParam := pq.QuoteLiteral(user.Name)
+	//_, err := tx.ExecContext(ctx, fmt.Sprintf("INSERT INTO m_user(user_id, user_name) VALUES(%s, %s)", userIdParam, userNameParam))
+
 	if err != nil {
 		return nil, err
 	}
