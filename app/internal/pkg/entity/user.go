30bdf5ee99b1cb275f670834a33aef693d5150f3
mysd33 <mysd.33@gmail.com>
2024-06-27 20:39:12 +0900

    誤記修正

--- app/internal/pkg/entity/user.go
+++ app/internal/pkg/entity/user.go
@@ -11,11 +11,11 @@ type User struct {
 	// ID は、ユーザのIDです。
 	ID string `json:"user_id" dynamodbav:"user_id"`
 	// Nameは、ユーザ名です。
-	Name string `json:"user_name" dynamodbav:"user_id"`
+	Name string `json:"user_name" dynamodbav:"user_name`
 }
 
 // GetKey DynamoDBのキー情報を取得します。
-func (user User) GetKey() (map[string]types.AttributeValue, error) {
+unc (user User) GetKey() (map[string]types.AttributeValue, error) {
 	id, err := attributevalue.Marshal(user.ID)
 	if err != nil {
 		return nil, err
