79a77ffd84b7c5c933197818a1bd4f6940d52f80
mysd33 <mysd.33@gmail.com>
2024-08-11 22:34:49 +0900

    変数名のリファクタリング

--- appbase/pkg/message/message_source.go
+++ appbase/pkg/message/message_source.go
@@ -13,41 +13,41 @@ import (
 
 var (
 	//go:embed messages_fw.yaml
-	messages_fw_yaml []byte
+	fwMessagesBytes []byte
 )
 
 // MessageSource は、メッセージを取得するインタフェースです。
 type MessageSource interface {
 	// GetMessage は、メッセージID（id）に対応し、置換文字列argsを設定したするメッセージを取得します。
 	GetMessage(id string, args ...any) string
-	// Add は業務APのメッセージのyaml定義(messages_yml)を、MessageSourceに追加します。
-	Add(messages_yaml []byte) error
+	// Add は業務APのメッセージのyaml定義(appMessagesBytes)を、MessageSourceに追加します。
+	Add(appMessagesBytes []byte) error
 }
 
 // defaultMessageSource は MessageSourceを実装する構造体です。
 type defaultMessageSource struct {
-	fwMessages map[string]string
-	messages   map[string]string
+	fwMessages  map[string]string
+	appMessages map[string]string
 }
 
 // NewMessageSource は、MessageSourceを作成します。
 func NewMessageSource() (MessageSource, error) {
 	//フレームワークのメッセージ定義（messages_fw.yaml）作成する
 	var fwMessages map[string]string
-	err := yaml.Unmarshal(messages_fw_yaml, &fwMessages)
+	err := yaml.Unmarshal(fwMessagesBytes, &fwMessages)
 	if err != nil {
 		return nil, err
 	}
-	return &defaultMessageSource{fwMessages: fwMessages, messages: map[string]string{}}, nil
+	return &defaultMessageSource{fwMessages: fwMessages, appMessages: map[string]string{}}, nil
 }
 
 // Add implements MessageSource.
-func (ms *defaultMessageSource) Add(messages_yaml []byte) error {
-	var newMessages map[string]string
-	if err := yaml.Unmarshal(messages_yaml, &newMessages); err != nil {
+func (ms *defaultMessageSource) Add(appMessagesBytes []byte) error {
+	var appNewMessages map[string]string
+	if err := yaml.Unmarshal(appMessagesBytes, &appNewMessages); err != nil {
 		return err
 	}
-	maps.Copy(ms.messages, newMessages)
+	maps.Copy(ms.appMessages, appNewMessages)
 	return nil
 }
 
@@ -55,7 +55,7 @@ func (ms *defaultMessageSource) Add(messages_yaml []byte) error {
 func (ms *defaultMessageSource) GetMessage(id string, args ...any) string {
 	// メッセージIDに対するメッセージ（のテンプレート）を取得
 	template := ""
-	if val, ok := ms.messages[id]; ok {
+	if val, ok := ms.appMessages[id]; ok {
 		template = val
 	} else if val, ok := ms.fwMessages[id]; ok {
 		template = val
